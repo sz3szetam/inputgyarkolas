f148c046c4b9b69c457b4080c90ea89b9e315fc1
Szécsi Tamás Dávid <144216869+sz3szetam@users.noreply.github.com>
2023-10-17 22:09:42 +0200

    q
    q

---
+++ feladat.js
@@ -0,0 +1,21 @@
+let tomb = [1,2,3,4,5];
+
+function Kiiras()
+{
+    let tartalom = ``;
+    for(let i = 0; i < tomb.length; i++)
+    {
+    }
+
+
+}
+function Hozzaad()
+{
+    let ertek = document.getElementById("text").value;
+    console.log("ertek"+ ertek);
+    tomb.push(ertek);
+   
+    Kiiras()
+}
+
+Kiiras();
